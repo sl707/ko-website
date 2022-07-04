2b2e2f398ce2d5a785dbd544dd1dac636559c469
Sam <90901198+sl707@users.noreply.github.com>
2022-07-05 05:07:24 +0900

    added header

---
+++ src/data/navigation.js
@@ -0,0 +1,148 @@
+const centralSubmenu = [
+  {
+    name: '회장 인사',
+    url: 'link11'
+  },
+  {
+    name: '발자취',
+    url: 'link12'
+  },
+  {
+    name: '회칙',
+    url: 'link13'
+  },
+  {
+    name: '종훈',
+    url: 'link14'
+  },
+  {
+    name: '성시표기',
+    url: 'link15'
+  },
+  {
+    name: '장학금 지급',
+    url: 'link16'
+  },
+  {
+    name: '오시는 길',
+    url: 'link17'
+  }
+]
+
+const historySubmenu = [
+  {
+    name: '고을라/고말로',
+    url: 'link21'
+  },
+  {
+    name: '개국설화',
+    url: 'link22'
+  },
+  {
+    name: '유래',
+    url: 'link23'
+  },
+  {
+    name: '9대 파조',
+    url: 'link24'
+  },
+  {
+    name: '연원',
+    url: 'link25'
+  },
+  {
+    name: '삼성혈/왕위전',
+    url: 'link26'
+  },
+  {
+    name: '항렬표',
+    url: 'link27'
+  },
+  {
+    name: '종기 해설',
+    url: 'link28'
+  }
+]
+
+const memberSubmenu = [
+  {
+    name: '중앙회',
+    url: 'link31'
+  },
+  {
+    name: '고문/위원회',
+    url: 'link32'
+  },
+  {
+    name: '지방',
+    url: 'link33'
+  },
+  {
+    name: '중국',
+    url: 'link34'
+  },
+  {
+    name: '장학회',
+    url: 'link35'
+  },
+  {
+    name: '제주',
+    url: 'link36'
+  },
+  {
+    name: '역대 회장단',
+    url: 'link37'
+  }
+]
+
+const gallerySubmenu = [
+  {
+    name: '종회/이사회',
+    url: 'link41'
+  },
+  {
+    name: '큰행사',
+    url: 'link42'
+  },
+  {
+    name: '제향',
+    url: 'link43'
+  },
+  {
+    name: '기타',
+    url: 'link45'
+  }
+]
+
+export const navLinks = [
+  {
+    name: '중앙종문회',
+    url: 'link1',
+    submenu: centralSubmenu
+  },
+  {
+    name: '역사',
+    url: 'link2',
+    submenu: historySubmenu
+  },
+  {
+    name: '임원',
+    url: 'link3',
+    submenu: memberSubmenu
+  },
+  {
+    name: '소식/자료실',
+    url: 'link4',
+    submenu: gallerySubmenu
+  },
+  {
+    name: '종보',
+    url: 'link5',
+    submenu: []
+  },
+  {
+    name: '알림',
+    url: 'link6',
+    submenu: []
+  }
+]
--- src/pages/using-ssr.js
+++ src/pages/using-ssr.js
@@ -1,8 +1,8 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from 'react'
+import { Link } from 'gatsby'
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from '../components/layout'
+import Seo from '../components/seo'
 
 const UsingSSR = ({ serverData }) => {
   return (
@@ -13,16 +13,16 @@ const UsingSSR = ({ serverData }) => {
       </h1>
       <p>
         This page is rendered server side every time the page is requested.
-        Reload it to see a(nother) random photo from{" "}
+        Reload it to see a(nother) random photo from{' '}
         <code>dog.ceo/api/breed/shiba/images/random</code>:
       </p>
       <img
-        style={{ width: "320px", borderRadius: "var(--border-radius)" }}
+        style={{ width: '320px', borderRadius: 'var(--border-radius)' }}
         alt="A random dog"
         src={serverData.message}
       />
       <p>
-        To learn more, head over to our{" "}
+        To learn more, head over to our{' '}
         <a href="https://www.gatsbyjs.com/docs/reference/rendering-options/server-side-rendering/">
           documentation about Server Side Rendering
         </a>
@@ -35,20 +35,20 @@ const UsingSSR = ({ serverData }) => {
 
 export default UsingSSR
 
-export async function getServerData() {
+export async function getServerData () {
   try {
-    const res = await fetch(`https://dog.ceo/api/breed/shiba/images/random`)
+    const res = await fetch('https://dog.ceo/api/breed/shiba/images/random')
     if (!res.ok) {
-      throw new Error(`Response failed`)
+      throw new Error('Response failed')
     }
     return {
-      props: await res.json(),
+      props: await res.json()
     }
   } catch (error) {
     return {
       status: 500,
       headers: {},
-      props: {},
+      props: {}
     }
   }
 }
