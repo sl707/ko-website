f2643fa250ea8ae260a28a5010b5fdd8b53d8965
Samuel Lee <samuellee@lev-00197.apn.wlan.private.upenn.edu>
2023-09-27 17:21:45 -0400

    netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     siteUrl: 'https://gatsbystarterdefaultsource.gatsbyjs.io/'
   },
   plugins: [
+    'gatsby-plugin-netlify',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-image',
     'gatsby-plugin-styled-components',
