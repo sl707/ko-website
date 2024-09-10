9d2dcc589ae5fa58dacac4de25c30171c1436ca3
Samuel Lee <samuellee@Samuels-MacBook-Pro-8.local>
2024-09-10 15:30:47 +0900

    feat: news 90 to 103

--- src/templates/newspaper-page.js
+++ src/templates/newspaper-page.js
@@ -15,25 +15,33 @@ const NewspaperPage = ({ pageContext: { paper } }) => (
   <Layout pageTitle={'종보'} pageSubtitle={`${paper.newsNumber}호`}>
     <NewsWrapper>
       <NewsImage src={`/news/${paper.newsNumber}-1.${paper.newsImageType ?? 'jpg'}`} />
-      {!paper.newsFirstOnly && <>
-        <NewsImage src={`/news/${paper.newsNumber}-2.${paper.newsImageType ?? 'jpg'}`} />
-        <NewsImage src={`/news/${paper.newsNumber}-3.${paper.newsImageType ?? 'jpg'}`} />
-        <NewsImage src={`/news/${paper.newsNumber}-4.${paper.newsImageType ?? 'jpg'}`} />
-        <NewsImage src={`/news/${paper.newsNumber}-5.${paper.newsImageType ?? 'jpg'}`} />
-        <NewsImage src={`/news/${paper.newsNumber}-6.${paper.newsImageType ?? 'jpg'}`} />
-        <NewsImage src={`/news/${paper.newsNumber}-7.${paper.newsImageType ?? 'jpg'}`} />
-        <NewsImage src={`/news/${paper.newsNumber}-8.${paper.newsImageType ?? 'jpg'}`} />
-      </>}
-      {
-        !paper.newsFirstOnly && paper.newsDoublePages && <>
-          <NewsImage src={`/news/${paper.newsNumber}-9.${paper.newsImageType ?? 'jpg'}`} />
-          <NewsImage src={`/news/${paper.newsNumber}-10.${paper.newsImageType ?? 'jpg'}`} />
-          <NewsImage src={`/news/${paper.newsNumber}-11.${paper.newsImageType ?? 'jpg'}`} />
-          <NewsImage src={`/news/${paper.newsNumber}-12.${paper.newsImageType ?? 'jpg'}`} />
-          <NewsImage src={`/news/${paper.newsNumber}-13.${paper.newsImageType ?? 'jpg'}`} />
-          <NewsImage src={`/news/${paper.newsNumber}-14.${paper.newsImageType ?? 'jpg'}`} />
-          <NewsImage src={`/news/${paper.newsNumber}-15.${paper.newsImageType ?? 'jpg'}`} />
-          <NewsImage src={`/news/${paper.newsNumber}-16.${paper.newsImageType ?? 'jpg'}`} />
+      {!paper.newsFirstOnly && 
+        <>
+          <NewsImage src={`/news/${paper.newsNumber}-2.${paper.newsImageType ?? 'jpg'}`} />
+          <NewsImage src={`/news/${paper.newsNumber}-3.${paper.newsImageType ?? 'jpg'}`} />
+          <NewsImage src={`/news/${paper.newsNumber}-4.${paper.newsImageType ?? 'jpg'}`} />
+          <NewsImage src={`/news/${paper.newsNumber}-5.${paper.newsImageType ?? 'jpg'}`} />
+          <NewsImage src={`/news/${paper.newsNumber}-6.${paper.newsImageType ?? 'jpg'}`} />
+          <NewsImage src={`/news/${paper.newsNumber}-7.${paper.newsImageType ?? 'jpg'}`} />
+          <NewsImage src={`/news/${paper.newsNumber}-8.${paper.newsImageType ?? 'jpg'}`} />
+          {
+            paper.newsNumPages >= 12 &&
+            <>
+              <NewsImage src={`/news/${paper.newsNumber}-9.${paper.newsImageType ?? 'jpg'}`} />
+              <NewsImage src={`/news/${paper.newsNumber}-10.${paper.newsImageType ?? 'jpg'}`} />
+              <NewsImage src={`/news/${paper.newsNumber}-11.${paper.newsImageType ?? 'jpg'}`} />
+              <NewsImage src={`/news/${paper.newsNumber}-12.${paper.newsImageType ?? 'jpg'}`} />
+            </>
+          }
+          {
+            paper.newsNumPages >= 16 &&
+            <>
+              <NewsImage src={`/news/${paper.newsNumber}-13.${paper.newsImageType ?? 'jpg'}`} />
+              <NewsImage src={`/news/${paper.newsNumber}-14.${paper.newsImageType ?? 'jpg'}`} />
+              <NewsImage src={`/news/${paper.newsNumber}-15.${paper.newsImageType ?? 'jpg'}`} />
+              <NewsImage src={`/news/${paper.newsNumber}-16.${paper.newsImageType ?? 'jpg'}`} />
+            </>
+          }
         </>
       }
     </NewsWrapper>
